bc6181bca733b0367e7bc6e35e80a8aae4e9edf9
Klever Barahona <kbarahon@c3r11s6.42madrid.com>
2020-09-14 12:58:02 +0200

    Make_and_h

---
+++ ft_printf.h
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kbarahon <kbarahon@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/14 12:51:49 by kbarahon          #+#    #+#             */
+/*   Updated: 2020/09/14 12:55:04 by kbarahon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdio.h>
+# include <stdarg.h>
+# include <limits.h>
+
+typedef struct	s_n
+{
+	long long int	*arg;
+	long long int	nchr;
+	struct s_n		*prev;
+	struct s_n		*next;
+}				t_n;
+
+typedef struct	s_convspecs
+{
+	char			sign;
+	char			adj;
+	char			padd;
+	char			alt;
+	int				width;
+	int				pre;
+	char			len;
+	char			spec;
+}				t_convspecs;
+
+#endif
